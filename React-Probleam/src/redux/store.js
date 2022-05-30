4b4d8b94348f6d0ce6f4fcfd4eddc8f41ba0cb09
Bvaishnavi123 <97448974+Bvaishnavi123@users.noreply.github.com>
2022-05-30 21:43:47 +0530

    updated

---
+++ React-Probleam/src/redux/store.js
@@ -0,0 +1,13 @@
+import {legacy_createStore} from "redux"
+import { combineReducers ,applyMiddleware} from "redux";
+import thunk from "redux-thunk";
+import { CityReducer } from "./CityAction/reducer";
+import { CountryReducer } from "./CountryAction/reducer";
+
+const rootReducer = combineReducers({
+    country : CountryReducer,
+    city:CityReducer
+})
+const store = legacy_createStore(rootReducer,applyMiddleware(thunk));
+export {store}
+
