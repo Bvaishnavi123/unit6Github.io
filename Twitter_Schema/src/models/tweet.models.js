ed1479ea9552de87a3c23dae0944084a496e140f
Bvaishnavi123 <97448974+Bvaishnavi123@users.noreply.github.com>
2022-06-05 01:20:40 +0530

    updated

---
+++ Twitter_Schema/src/models/tweet.models.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+// POST SCHEMA
+// Step 1 :- creating the schema
+const postSchema = new mongoose.Schema(
+  {
+   
+    tweet: { type: String, required: true },
+    tweetId:{type:Number,require:true,unique:true},
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true, // createdAt, updatedAt
+  }
+);
+
+// Step 2 :- creating the model
+module.exports = mongoose.model("post", postSchema); // post => posts
---
+++ Twitter_Schema/src/models/user.models.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+// USER SCHEMA
+// Step 1 :- creating the schema
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: false },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    followers:{type:Number,required:true},
+    following:{type:Number,required:true},
+    personalInfo:{ type: String, required: true }
+  },
+  {
+    versionKey: false,
+    timestamps: true, // createdAt, updatedAt
+  }
+);
+
+// Step 2 : creating the model
+const User = mongoose.model("user", userSchema); // user => users
+
+module.exports = User;
