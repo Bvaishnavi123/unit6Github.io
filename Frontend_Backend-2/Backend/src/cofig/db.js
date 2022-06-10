1c5830aa2efce218a0fb6ba0b2177da251b6add5
Bvaishnavi123 <97448974+Bvaishnavi123@users.noreply.github.com>
2022-06-11 01:55:26 +0530

    updated

--- Frontend_Backend-2/Backend/src/cofig/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const connect = ()=>{
-    return mongoose.connect('mongodb+srv://vaishnavi:vaishnavi@cluster0.3njvxok.mongodb.net/product?retryWrites=true&w=majority')
-}
-
-module.exports = connect
